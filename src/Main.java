13951c57be811f0827ddac364e506f02216e0584
WaseemAkhtar032 <waseemakhtar032@gmail.com>
2023-01-23 12:12:26 +0530

    Added: Welcome to Employee Wage Computation!

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import uc4.EmployeeWageCase;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(" WELCOME TO EMPLOYEE WAGE COMPUTATION \n ");
+        EmployeeWageCase s = new EmployeeWageCase();
+        s.employeeWageChoice();
+    }
+}
